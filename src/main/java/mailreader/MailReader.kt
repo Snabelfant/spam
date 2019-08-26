2ce90313740b6455f3e79ee2a2253a2af2a7db38
dmvv <dmvv@start.no>
2019-08-26 23:16:32 +0200

    Initial commit

---
+++ src/main/java/mailreader/MailReader.kt
@@ -0,0 +1,14 @@
+package mailreader
+
+    fun main( args : Array<String>) {
+
+    }
+
+
+class MailReader(server: String, port: String, username: String, password: String){
+
+    fun readMails(){
+        
+    }
+
+}
